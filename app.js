148e91fa1a0ecd07fc3740096cce8dfa4e57d4e3
Mary McGeary <malicemcgeary@gmail.com>
2019-03-18 20:30:27 -0400

    email and phone links better but not responsive yet

--- app.js
+++ app.js
@@ -71,6 +71,7 @@ window.onclick = function(event) {   // close modal when click anywhere on scree
 //project description stuff
 const projectDescriptions = document.getElementsByClassName("projectDescription");
 const projectButtons = document.getElementsByClassName("projectButton");
+
 for (let i = 0; i < projectDescriptions.length; i++) {  //so descriptions don't start off open
   projectDescriptions[i].style.display = "none";
 }
@@ -91,14 +92,17 @@ for (let i=0; i < projectButtons.length; i++) {
 }
 
 // contact icon image clicks
-const email = document.querySelector("#email");
-email.addEventListener("click", function() {
-  email.innerHTML = "mmcgeary1@gmail.com";
-});
 
-const phone = document.querySelector("#phone");
-phone.addEventListener("click", function() {
-  phone.innerHTML = "(216)618-0230";
-});
+
+
+// const email = document.querySelector("#email");
+// email.addEventListener("click", function() {
+//   email.innerHTML = "mmcgeary1@gmail.com";
+// });
+
+// const phone = document.querySelector("#phone");
+// phone.addEventListener("click", function() {
+//   phone.innerHTML = "(216)618-0230";
+// });
 
 })();
